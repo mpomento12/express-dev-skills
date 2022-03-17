6d59ccba683306dabd4cedbfd8240d699cb44ec2
mpomento <mpomento@gmail.com>
2022-03-17 12:12:40 -0700

    changes to the project

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,18 +1,31 @@
+const res = require('express/lib/response');
 const Skill = require('../models/skill');
 
 module.exports = {
     index,
     show,
+    new: newSkills,
+    create,
   };
   
-  function index(req, res) {
-    res.render('skills/index', {
-      skills: Skill.getAll()
-    });
-  };
+function index(req, res) {
+  res.render('skills/index', {
+    skills: Skill.getAll()
+  });
+};
 
-  function show(req, res) {
-    res.render('skills/show', {
-      skill: Skill.getOne(req.params.id),
-    });
-  };
+function show(req, res) {
+  res.render('skills/show', {
+    skill: Skill.getOne(req.params.id),
+  });
+};
+
+function newSkills(req, res) {
+  res.render('skills/new');
+};
+
+function create(req, res) {
+  console.log(req.body);
+  Skill.create(req.body);
+  res.redirect('/skills');
+};
--- models/skill.js
+++ models/skill.js
@@ -1,12 +1,13 @@
 const skills = [
-  {id: 120469, rapper: 'Jay-Z', goat: true},
-  {id: 091473, rapper: 'Nas', goat: true},
-  {id: 121283, rapper: 'Soulja-Boy', goat: false}
+  {id: 120469, skill: 'Jay-Z', goat: true},
+  {id: 091473, skill: 'Nas', goat: true},
+  {id: 121283, skill: 'Soulja-Boy', goat: false}
 ];
 
 module.exports = {
   getAll,
-  getOne
+  getOne,
+  create
 };
 
 function getAll() {
@@ -16,4 +17,10 @@ function getAll() {
 function getOne(id) {
   id = parseInt(id);
   return skills.find(skill => skill.id === id)
+}
+
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.done = false;
+  skills.push(skill);
 }
